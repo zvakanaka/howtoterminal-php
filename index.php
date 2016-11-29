87121451aada5df3b6e0c0091d8b7d8aa8486df5
Adam Quinton <quintonish@gmail.com>
2016-11-29 11:07:19 -0800

    Add system and index

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php require('php-class/dynamic/lib/string_tools.php'); ?>
+<?php session_start(); ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/parts/head.php'; ?>
+<main>
+<article>
+	<a href="/php-class/assignments.php">Exercises (PHP Class)</a>
+</article>
+</main>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/views/parts/toes.php'; ?>
