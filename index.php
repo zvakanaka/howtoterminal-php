26f42b712d22a2afbdaa85ec1675270383da6ae2
Adam Quinton <quintonish@gmail.com>
2016-12-08 17:10:54 -0700

    Add youtube embed

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 <?php session_start(); ?>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/views/parts/head.php'; ?>
 <main>
-<article>
 	<a href="/php-class/assignments.php">Exercises (PHP Class)</a>
-</article>
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/RNfa17T6CVA" frameborder="0" allowfullscreen></iframe>
 </main>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/views/parts/toes.php'; ?>
