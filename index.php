55243ddc1efec18f950ec4758fa0a223335b7120
Adam Quinton <quintonish@gmail.com>
2016-12-08 17:19:08 -0700

    Fix

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php session_start(); ?>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/views/parts/head.php'; ?>
 <main>
-	<a href="/php-class/assignments.php">Exercises (PHP Class)</a>
 	<iframe width="560" height="315" src="https://www.youtube.com/embed/RNfa17T6CVA" frameborder="0" allowfullscreen></iframe>
+	<br><a href="/php-class/assignments.php">Exercises (PHP Class)</a>
 </main>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/views/parts/toes.php'; ?>
